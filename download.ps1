082e24e0a1f7d90e24b4059a20625fe3acd1eb10
Jadyn <jadyn.wong@live.com>
2019-04-16 14:18:17 +0800

    sync cz88

--- download.ps1
+++ download.ps1
@@ -1,7 +1,8 @@
+$rootFolder = (Get-Item -Path "./" -Verbose).FullName
 $copywrite_src = 'http://update.cz88.net/ip/copywrite.rar'
 $qqwry_src = 'http://update.cz88.net/ip/qqwry.rar'
-$copywrite_des = "$env:temp\copywrite.rar"
-$qqwry_des = "$env:temp\qqwry.rar"
+$copywrite_des = (Join-Path $rootFolder "copywrite.rar")
+$qqwry_des = (Join-Path $rootFolder "qqwry.rar")
 Invoke-WebRequest -uri $copywrite_src -OutFile $copywrite_des
 Unblock-File $copywrite_des
 Invoke-WebRequest -uri $qqwry_src -OutFile $qqwry_des
